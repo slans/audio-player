e428f1870f8f201b015e7a888d6f7eca2b597f56
Slava Antonenko <slans2@gmail.com>
2020-09-11 14:47:10 +0300

    Условный рендеринг

--- src/App.js
+++ src/App.js
@@ -1,24 +1,102 @@
 import React, { Component } from 'react';
 
-class App extends Component {
+const WarningBanner = props => {
+  if (!props.warn) return null;
+  return (
+    <div className='warning'>
+      Предупреждение!
+    </div>
+  )
+}
+
+const Mailbox = props => {
+  const messages = props.messages;
+  return (
+    <>
+      <h1>Здравствуйте</h1>
+      {messages.length > 0 && <h2>У вас непрочитанных сообщений {messages.length}</h2>}
+    </>
+  )
+}
+
+const LoginButton = props => {
+  return (
+    <button onClick={props.onClick}>
+      Залогиниться
+    </button>
+  )
+}
+
+const LogoutButton = props => {
+  return (
+    <button onClick={props.onClick}>
+      Вылогиниться
+    </button>
+  )
+}
+
+const Greeting = props => {
+  return (
+    <p>Hello {props.isLoggedIn ? 'client' : 'guest'}</p>
+  )
+}
+
+class LoginControl extends Component {
   state = {
-    isToogleOn: false
+    isLoggedIn: false
   }
 
-  handleClick = () => {
+  handleLogin = () => {
     this.setState(state => ({
-      isToogleOn: !state.isToogleOn
+      isLoggedIn: true
+    }))
+  }
+
+  handleLogout = () => {
+    this.setState(state => ({
+      isLoggedIn: false
     }))
   }
 
   render() {
+    const isLoggedIn = this.state.isLoggedIn;
+    const button = isLoggedIn
+      ? <LogoutButton onClick={this.handleLogout} />
+      : <LoginButton onClick={this.handleLogin} />
+
     return (
-      <button onClick={this.handleClick}>
-        {this.state.isToogleOn 
-          ? 'Включено'
-          : 'Выключено'
-        }
-      </button>
+      <>
+        <Greeting isLoggedIn={isLoggedIn} />
+        {button}
+      </>
+    )
+  }
+}
+
+class App extends Component {
+  state = {
+    isWarning: true
+  }
+
+  handleClick = () => {
+    this.setState(state => ({
+      isWarning: !state.isWarning
+    }))
+  }
+
+  render() {
+    const showWarning = this.state.isWarning;
+    const messagesArr = ['Привет', 'Как дела?', 'Где был?']
+
+    return (
+      <>
+        <WarningBanner warn={showWarning} />
+        <button onClick={this.handleClick}>
+          {showWarning ? 'Скрыть' : 'Показать'}
+        </button>
+        <Mailbox messages={messagesArr} />
+        <LoginControl/>
+      </>
     )
   }
 }
