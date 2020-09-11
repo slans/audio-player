7fc894301f24c173cd549209731ef58b2a3399d3
Slava Antonenko <slans2@gmail.com>
2020-09-11 12:06:11 +0300

    Состояние и жизненный цикл

---
+++ src/Clock.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+
+class Clock extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      date: new Date()
+    }
+  }
+
+  componentDidMount() {
+    this.timerId = setInterval(() => {
+      this.tick();
+    }, 1000)
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerId);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    })
+  }
+
+
+  render() {
+    return (
+    <p>Current time is {this.state.date.toLocaleTimeString()}</p>
+    )
+  }
+}
+
+export default Clock;
