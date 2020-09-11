32af3577b21d1a1831dc820f40cac92af06744ab
Slava Antonenko <slans2@gmail.com>
2020-09-11 15:54:44 +0300

    Списки и ключи

--- src/App.js
+++ src/App.js
@@ -1,101 +1,29 @@
 import React, { Component } from 'react';
 
-const WarningBanner = props => {
-  if (!props.warn) return null;
-  return (
-    <div className='warning'>
-      Предупреждение!
-    </div>
-  )
-}
-
-const Mailbox = props => {
-  const messages = props.messages;
-  return (
-    <>
-      <h1>Здравствуйте</h1>
-      {messages.length > 0 && <h2>У вас непрочитанных сообщений {messages.length}</h2>}
-    </>
-  )
-}
-
-const LoginButton = props => {
-  return (
-    <button onClick={props.onClick}>
-      Залогиниться
-    </button>
-  )
-}
-
-const LogoutButton = props => {
-  return (
-    <button onClick={props.onClick}>
-      Вылогиниться
-    </button>
-  )
-}
-
-const Greeting = props => {
-  return (
-    <p>Hello {props.isLoggedIn ? 'client' : 'guest'}</p>
-  )
-}
-
-class LoginControl extends Component {
-  state = {
-    isLoggedIn: false
-  }
-
-  handleLogin = () => {
-    this.setState(state => ({
-      isLoggedIn: true
-    }))
-  }
-
-  handleLogout = () => {
-    this.setState(state => ({
-      isLoggedIn: false
-    }))
-  }
-
-  render() {
-    const isLoggedIn = this.state.isLoggedIn;
-    const button = isLoggedIn
-      ? <LogoutButton onClick={this.handleLogout} />
-      : <LoginButton onClick={this.handleLogin} />
-
-    return (
-      <>
-        <Greeting isLoggedIn={isLoggedIn} />
-        {button}
-      </>
-    )
-  }
-}
 
 class App extends Component {
-  state = {
-    isWarning: true
-  }
-
-  handleClick = () => {
-    this.setState(state => ({
-      isWarning: !state.isWarning
-    }))
-  }
 
   render() {
-    const showWarning = this.state.isWarning;
-    const messagesArr = ['Привет', 'Как дела?', 'Где был?']
+    const posts = this.props.posts;
+    const sidebar = posts.map(entry => {
+      return <li key={entry.id}>{entry.title}</li>
+    });
+
+    const content = posts.map(entry => {
+      return (
+        <div key={entry.index}>
+          <span>{entry.title}</span>
+          <span>{entry.content}</span>
+        </div>
+      )
+    });
 
     return (
       <>
-        <WarningBanner warn={showWarning} />
-        <button onClick={this.handleClick}>
-          {showWarning ? 'Скрыть' : 'Показать'}
-        </button>
-        <Mailbox messages={messagesArr} />
-        <LoginControl/>
+        <ul>
+          {sidebar}
+        </ul>
+        {content}
       </>
     )
   }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+const posts = [
+  { id: 1, title: 'Привет, мир', content: 'Добро пожаловать в документацию React!' },
+  { id: 2, title: 'Установка', content: 'React можно установить из npm.' }
+];
+
 ReactDOM.render(
-  <App/>,
+  <App posts={posts}/>,
   document.getElementById('root')
 );
