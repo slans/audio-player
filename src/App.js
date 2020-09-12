467dbbb82885f2766651cedf357d26a0a2722c33
Slava Antonenko <slans2@gmail.com>
2020-09-12 14:08:44 +0300

    Формы

--- src/App.js
+++ src/App.js
@@ -2,29 +2,47 @@ import React, { Component } from 'react';
 
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isGoing: true,
+      numberOfGuests: 0
+    }
+  }
+
+  handleChange = (e) => {
+    const target = e.target;
+    const name = target.name;
+    const value = name === 'isGoing' ? target.checked : target.value;
+    this.setState({
+      [name]: value
+    })
+  }
 
   render() {
-    const posts = this.props.posts;
-    const sidebar = posts.map(entry => {
-      return <li key={entry.id}>{entry.title}</li>
-    });
-
-    const content = posts.map(entry => {
-      return (
-        <div key={entry.index}>
-          <span>{entry.title}</span>
-          <span>{entry.content}</span>
-        </div>
-      )
-    });
 
     return (
-      <>
-        <ul>
-          {sidebar}
-        </ul>
-        {content}
-      </>
+      <form>
+        <label>
+          Пойдут:
+          <input
+            name="isGoing"
+            type="checkbox"
+            checked={this.state.isGoing}
+            onChange={this.handleChange}
+          />
+        </label>
+        <br />
+        <label>
+          Количество гостей:
+          <input
+            name="numberOfGuests"
+            type="number"
+            value={this.state.numberOfGuests}
+            onChange={this.handleChange}
+          />
+        </label>
+      </form>
     )
   }
 }
