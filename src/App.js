b15619a13dc54e8a5aa8e6807f6fa5aa34b8fef3
Slava Antonenko <slans2@gmail.com>
2020-09-14 12:19:56 +0300

    Подъём состояния

--- src/App.js
+++ src/App.js
@@ -1,50 +1,95 @@
 import React, { Component } from 'react';
 
+const fahrenheitToCelsius = temperature => {
+  return (temperature - 32) / 1.8;
+}
+const celsiusToFahrenheit = temperature => {
+  return (temperature * 1.8) + 32;
+}
+
+const convertTemp = (temperature, convertFunc) => {
+  if (Number.isNaN(temperature)) {
+    return '';
+  }
+  return Math.round(convertFunc(temperature));
+}
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isGoing: true,
-      numberOfGuests: 0
-    }
+  render() {
+    return (
+      <Calculator />
+    )
+  }
+}
+
+class Calculator extends Component {
+  state = {
+    temperature: 0,
+    scale: 'c'
   }
 
-  handleChange = (e) => {
-    const target = e.target;
-    const name = target.name;
-    const value = name === 'isGoing' ? target.checked : target.value;
+  handleCelsiusChange = temperature => {
     this.setState({
-      [name]: value
+      temperature,
+      scale: 'c'
+    })
+  }
+
+  handleFarenheitChange = temperature => {
+    this.setState({
+      temperature,
+      scale: 'f'
     })
   }
 
   render() {
+    const { scale, temperature } = this.state;
+    const celsiusTemp = scale === 'c' ? temperature : convertTemp(temperature, fahrenheitToCelsius);
+    const fahrenheitTemp = scale === 'f' ? temperature : convertTemp(temperature, celsiusToFahrenheit);
 
     return (
-      <form>
-        <label>
-          Пойдут:
-          <input
-            name="isGoing"
-            type="checkbox"
-            checked={this.state.isGoing}
-            onChange={this.handleChange}
-          />
-        </label>
-        <br />
-        <label>
-          Количество гостей:
-          <input
-            name="numberOfGuests"
-            type="number"
-            value={this.state.numberOfGuests}
-            onChange={this.handleChange}
-          />
-        </label>
-      </form>
+      <>
+        <TemperatureInput
+          scale='c'
+          temperature={celsiusTemp}
+          handleChange={this.handleCelsiusChange} />
+        <TemperatureInput
+          scale='f'
+          temperature={fahrenheitTemp}
+          handleChange={this.handleFarenheitChange} />
+        <BoilingVerdict temperature={celsiusTemp} />
+      </>
     )
   }
+}
+
+const TemperatureInput = props => {
+  const scaleNames = {
+    'c': 'Celsius',
+    'f': 'Fahrenheit'
+  }
+
+  const handleChange = e => {
+    const value = e.target.value;
+    if (isNaN(value)) return;
+    props.handleChange(value);
+  }
+
+  return (
+    <fieldset>
+      <legend>Введите градусы по шкале {scaleNames[props.scale]}:</legend>
+      <input value={props.temperature}
+        onChange={handleChange} />
+    </fieldset>
+  )
+}
+
+const BoilingVerdict = props => {
+  return (
+    props.temperature > 100
+      ? <p>The water is boiling!</p>
+      : <p>The water would not boil</p>
+  )
 }
 
 export default App;
