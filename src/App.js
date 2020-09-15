c06c7b7d0ca3088d9d955c2103fcc19c197ad865
Slava Antonenko <slans2@gmail.com>
2020-09-15 11:29:16 +0300

    Философия React

--- src/App.js
+++ src/App.js
@@ -1,58 +1,113 @@
 import React, { Component } from 'react';
 import './styles.css';
 
-const FancyBorder = props => {
-  return (
-    <div className={`FancyBorder FancyBorder-${props.color}`}>
-      {props.children}
-    </div>
-  )
-}
-
-const Dialog = props => {
-  return (
-    <FancyBorder color='blue'>
-      <h1 className='Dialog-title'>
-        {props.title}
-      </h1>
-      <p className='Dialog-message'>
-        {props.message}
-      </p>
-      {props.children}
-    </FancyBorder>
-  )
-}
-
 class App extends Component {
   state = {
-    login: ''
+    filteredText: '',
+    inStockOnly: false
   }
 
-  handleChange = (e) => {
+  handleFilteredText = (e) => {
     const value = e.target.value;
     this.setState({
-      login: value
-    })
+      filteredText: value
+    });
   }
 
-  handleSubmit = () => {
-    alert(`Welcome on board, ${this.state.login}`)
+  handleInStock = (e) => {
+    const value = e.target.checked;
+    this.setState({
+      inStockOnly: value
+    });
   }
 
   render() {
-    const {login} = this.state;
+    const { filteredText, inStockOnly } = this.state;
+    let data = this.props.data;
+    // if (filteredText) {
+    //   const filteredData = data.filter(entry => entry.name.contains(filteredText));
+    //   data = filteredData;
+    // }
+
     return (
-      <Dialog title='Mars Exploration Program'
-              message='How should we refer to you?'>
-        <input type='text'
-               value={login} 
-               onChange={this.handleChange}/>
-        <button onClick={this.handleSubmit}>
-          Sign me up
-        </button>
-      </Dialog>
+      <>
+        <SearchBar 
+          value={filteredText} 
+          inStockOnly={inStockOnly} 
+          handleFilteredText={this.handleFilteredText} 
+          handleInStock={this.handleInStock} />
+        <FilteredTable data={data} filteredText={filteredText} inStockOnly={inStockOnly} />
+      </>
     )
   }
+}
+
+const SearchBar = props => {
+  return (
+    <>
+      <input type='text' value={props.value} onChange={props.handleFilteredText} />
+      <input id='getStock' type='checkbox' checked={props.inStockOnly} onChange={props.handleInStock} />
+      <label htmlFor='getStock'>Only shows products in stock</label>
+    </>
+  )
+}
+
+const FilteredTable = props => {
+  const {data, filteredText, inStockOnly} = props;
+  const rows = [];
+  let lastCategory = null;
+
+  data.forEach(entry => {
+    let category = entry.category;
+    if (inStockOnly && !entry.stocked) {
+      return;
+    }
+    if (!entry.name.includes(filteredText)) {
+      return;
+    }
+    if (category !== lastCategory) {
+      rows.push(<ProductCategory category={category} />);
+    }
+
+    rows.push(<ProductRow product={entry} />)
+    lastCategory = category;
+  });
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Price</th>
+        </tr>
+      </thead>
+      <tbody>
+        {rows}
+      </tbody>
+    </table>
+  )
+}
+
+const ProductCategory = props => {
+  return (
+    <tr>
+      <th colSpan={2} className='category'>{props.category}</th>
+    </tr>
+  )
+}
+
+const ProductRow = props => {
+  const product = props.product;
+  const name = product.stocked
+    ? product.name
+    : <span style={{ color: 'red' }}>{product.name}</span>
+
+  return (
+    <tr>
+      <td>{name}</td>
+      <td>{product.price}</td>
+    </tr>
+  )
 }
 
 export default App;
